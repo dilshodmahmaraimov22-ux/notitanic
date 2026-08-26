55f791c1abe181ab1a1d0f5148c0b5b5ab939514
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-08-26 19:53:03 +0500

    bog'ladim

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import './App.css'
+import Hero from './Components/Hero/Hero'
 
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   )
 }

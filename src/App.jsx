7a7a95a4042cbc520e3e03a19739667efd0742fe
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-08-27 18:07:08 +0500

    carc tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Components/Header/Header'
 import './App.css'
 import Hero from './Components/Hero/Hero'
+import Card from './Components/Card/Card'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <div>
       <Header/>
       <Hero/>
+      <Card/>
     </div>
   )
 }
